940e33f1b8baa0654d45cc63e25cf1ee0337c8d1
Justin Deutsch <justindeu22@gmail.com>
2020-02-21 17:43:15 +0000

    timezone update and reauth

--- nwc.py
+++ nwc.py
@@ -4,8 +4,11 @@ import logging
 import time
 import numpy as np
 from datetime import datetime
+from pytz import timezone
 from bs4 import BeautifulSoup
 from oauth2client.service_account import ServiceAccountCredentials
+
+eastern = timezone('US/Eastern')
 
 logging.basicConfig(
     filename='app.log',
@@ -48,6 +51,22 @@ class School():
     def past_hour(self):
         return sum(self.diffs)
 
+def auth_sheet():
+    global eastern
+    scope = [
+        'https://spreadsheets.google.com/feeds',
+        'https://www.googleapis.com/auth/drive'
+    ]
+
+    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials_sheets.json', scope)
+
+    client = gspread.authorize(creds)
+
+    sheet = client.open('NWC Updater').sheet1
+    logging.info('Google Sheet Authorized')
+    sheet.update_acell('B12', str(datetime.now(eastern)))
+    return sheet
+
 
 schools = {
     'VT': School('VT', 2, 'https://secure.acsevents.org/site/TR/RelayForLife/RFLCY20SER?pg=entry&fr_id=95544', 200746),
@@ -60,39 +79,37 @@ schools = {
 }
 
 logging.info('Initialzing connections to Google Sheet...')
-
-scope = [
-    'https://spreadsheets.google.com/feeds',
-    'https://www.googleapis.com/auth/drive'
-]
-
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials_sheets.json', scope)
-
-client = gspread.authorize(creds)
-
-sheet = client.open('NWC Updater').sheet1
-
+sheet = auth_sheet()
 logging.info('Connection to Google Sheet Initialized!')
-sheet.update_acell('B10', str(datetime.now()))
 
+# updating running since cell
+sheet.update_acell('B10', str(datetime.now(eastern)))
+
+
+# Loop to update sheet
 while True:
 
     logging.info('Beginning Update...')
 
-    school_data = sheet.range('A2:D8')
-    school_data = np.reshape(school_data, (len(schools), -1))
+    try:
+        school_data = sheet.range('A2:D8')
+        school_data = np.reshape(school_data, (len(schools), -1))
 
-    for row in school_data:
-        school = schools[row[0].value]
-        school.update_total()
+        for row in school_data:
+            school = schools[row[0].value]
+            school.update_total()
 
-        row[1].value = f'{school.nwc_amount():.2f}'
-        row[2].value = f'{school.curr_amount:.2f}'
-        row[3].value = f'{school.past_hour():.2f}'
+            row[1].value = f'{school.nwc_amount():.2f}'
+            row[2].value = f'{school.curr_amount:.2f}'
+            row[3].value = f'{school.past_hour():.2f}'
 
 
-    sheet.update_cells(list(np.ravel(school_data)))
-    sheet.update_acell('B11', str(datetime.now()))
+        sheet.update_cells(list(np.ravel(school_data)))
+        sheet.update_acell('B11', str(datetime.now(eastern)))
 
-    logging.info('Update Completed!')
-    time.sleep(60)
+        logging.info('Update Completed!')
+        time.sleep(60)
+    except gspread.exceptions.APIError:
+        logging.error('GSpread APIError occurred, reauthorizing and trying again')
+        sheet = auth_sheet()
+        continue
